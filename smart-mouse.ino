33d80c5d50d7e319490dcfc3a68f0d1a7512bf85
Property404 <martinez.dagan@gmail.com>
2017-03-26 03:39:00 -0400

    Fixed hardware, rearranged some ports, progress on copying and pasting

--- smart-mouse.ino
+++ smart-mouse.ino
@@ -1,18 +1,21 @@
 /* Smart mouse code */
+#include <EEPROM.h>
 #define ARRAY_END 10
-const byte REQUEST_CLICK = 'C';
-const byte REQUEST_COPY = 'c';
-const byte REQUEST_PASTE = 'p';
-const byte REQUEST_MOVE = 'm';
+#define BYTE_LIMIT 1000
+#define REQUEST_CLICK  'C'
+#define REQUEST_COPY  'c'
+#define REQUEST_PASTE  'p'
+#define REQUEST_MOVE  'm'
+#define STATUS 's'
 
-const int BAUD_RATE = 9600;
+#define BAUD_RATE  9600;
 
-const int LEFT_BUTTON = 4;
-const int RIGHT_BUTTON = 3;
-const int COPY_BUTTON = 11;
-const int PASTE_BUTTON = 10;
-const int X_TURNER = A0;
-const int Y_TURNER = A1;
+#define LEFT_BUTTON  2
+#define RIGHT_BUTTON  4
+#define COPY_BUTTON  11
+#define PASTE_BUTTON  10
+#define X_TURNER  A0
+#define Y_TURNER  A1
 void setup()
 {
   // initialize serial:
@@ -43,21 +46,66 @@ void copy(){
    byte data [] = {REQUEST_COPY, ARRAY_END};
    Serial.write(data, 2);
 }
+void paste(){
+  byte data [1 + BYTE_LIMIT] = {'?'};
+  data[0] = REQUEST_PASTE;
+  // This loop makes sense
+  // R
+  byte j = 0;
+  int i;
+  for(i=0;j=EEPROM.read(i), i<BYTE_LIMIT; i++){
+    data[i+1] = j;
+    data[i+2] = ARRAY_END;
+  }
+  Serial.write(data, 1+i);
+}
+/*
+void store_clipboard(byte* computer_clipboard){
+  
+  for(int i=0; i<BYTE_LIMIT && computer_clipboard[i]!=0 && computer_clipboard[i]!=ARRAY_END; i++){
+    EEPROM.write(i, computer_clipboard[i]);
+  }
+}*/
 
+void store_iff_can(){
+  int i = 0;
+  int j = 0;
+  bool hit_zero = 0;
+  while(Serial.available() > 0){
+    j = Serial.read();
+    if(j==0){ hit_zero = true;}
+    
+    if(!hit_zero){
+    EEPROM.write(i, j);
+    i++;
+    }
+    if(j==ARRAY_END){ hit_zero = true;}
+  }
+  if(i>0){
+    byte data [] = {STATUS,i, i, ARRAY_END};
+    Serial.write(data, 4);
+  }
+}
 
 
 bool left_down = 0;
 bool right_down = 0;
+bool copy_down = 0;
+bool paste_down = 0;
 bool old_x = 0;
 bool old_y = 0;
 void loop()
 {
  bool left_raw = digitalRead(LEFT_BUTTON);
  bool right_raw = digitalRead(RIGHT_BUTTON);
+ bool copy_raw = digitalRead(COPY_BUTTON);
+ bool paste_raw = digitalRead(PASTE_BUTTON);
  int x_turner_raw = (analogRead(X_TURNER)) >> 2;
  int y_turner_raw = (analogRead(Y_TURNER)) >> 2;
  if(x_turner_raw<ARRAY_END+1)x_turner_raw=ARRAY_END+1;
  if(y_turner_raw<ARRAY_END+1)y_turner_raw=ARRAY_END+1;
+
+  store_iff_can();
  
  // RIGHT BUTTON
  if(right_raw && !right_down){
@@ -73,6 +121,22 @@ void loop()
   left_down = 1;
  }else if(!left_raw){
     left_down = 0;
+ }
+
+ // COPY BUTTON
+ if(copy_raw && !copy_down){
+    copy();
+    copy_down = 1;
+ }else if(!copy_raw){
+    copy_down = 0;
+ }
+
+ // PASTE BUTTON
+ if(paste_raw && !paste_down){
+    paste();
+    paste_down = 1;
+ }else if(!paste_raw){
+    paste_down = 0;
  }
 
  // X/Y-axis turner
